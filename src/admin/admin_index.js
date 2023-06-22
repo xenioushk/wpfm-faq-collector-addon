44981cf3f3ea95ba3debc2f413c91a78f39e85af
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-06-22 16:17:47 +0300

    implemented pipeline and wp-scripts

---
+++ src/admin/admin_index.js
@@ -0,0 +1,6 @@
+// Load Stylesheets.
+import "./styles/admin.scss"
+
+// Load JavaScripts
+// import "./modules/atfc_quick_bulk_edit"
+// import "./modules/atfc_download_report"
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Stylesheets
+import "./styles/frontend.scss"
+
+// Javascripts
+import "./modules/fca_custom"
+// import "./modules/kb_tabify"
+// import "./modules/kb_question_form"
+// import "./modules/kb_toc"
+// import "./modules/kb_all"
